39d05560fad8e050f9376aeb40b394a0103fa599
akarneliuk <a.karneliuk@gmail.com>
2022-02-13 12:42:04 +0000

    get_facts completed

--- napalm/gnmi/module_openconfig.py
+++ napalm/gnmi/module_openconfig.py
@@ -1,5 +1,6 @@
 # std libs
-import json
+import time
+
 # third party libs
 
 # Functions
@@ -11,11 +12,11 @@ def get_facts(gnmi_object, orgs) -> dict:
 
     # Get hostname
     try:
-        hostname = data["notification"][0]["update"][0]["val"]["openconfig-system:config"]["hostname"]
+        hostname = data["notification"][0]["update"][0]["val"]["openconfig-system:state"]["hostname"]
         
     except:
         try: 
-            hostname = data["notification"][0]["update"][0]["val"]["config"]["hostname"]
+            hostname = data["notification"][0]["update"][0]["val"]["state"]["hostname"]
 
         except:
             hostname = ""
@@ -24,10 +25,10 @@ def get_facts(gnmi_object, orgs) -> dict:
 
     # Get fqdn
     try:
-        domain = data["notification"][0]["update"][0]["val"]["openconfig-system:config"]["domain-name"]
+        domain = data["notification"][0]["update"][0]["val"]["openconfig-system:state"]["domain-name"]
     except:
         try: 
-            domain = data["notification"][0]["update"][0]["val"]["config"]["domain-name"]
+            domain = data["notification"][0]["update"][0]["val"]["state"]["domain-name"]
 
         except:
             domain = ""
@@ -39,30 +40,64 @@ def get_facts(gnmi_object, orgs) -> dict:
 
     vendor = known_vendors & orgs
 
-    result["vendor"] = vendor.pop()
+    result["vendor"] = vendor.pop().capitalize()
 
-    # commands = ["show version", "show hostname", "show interfaces"]
+    # Get Model
+    # TODO test against real device, as vEOS doesn't have HW type
+    model = ""
+    try:
+        for item in data["notification"][1]["update"][0]["val"]["openconfig-platform:component"]:
+            if item["name"] == "Chassis":
+                model = item["state"]["type"]
+    except:
+        try: 
+            for item in data["notification"][1]["update"][0]["val"]["component"]:
+                if item["name"] == "Chassis":
+                    model = item["state"]["type"]
+        except:
+            model = ""
 
-    # result = self.device.run_commands(commands)
+    result["model"] = model
 
-    # version = result[0]
-    # hostname = result[1]
-    # interfaces_dict = result[2]["interfaces"]
+    # Get serial_number
+    # TODO test against real device, as vEOS doesn't have SN
+    serial_number = ""
 
-    # uptime = time.time() - version["bootupTimestamp"]
+    result["serial_number"] = serial_number
 
-    # interfaces = [i for i in interfaces_dict.keys() if "." not in i]
-    # interfaces = string_parsers.sorted_nicely(interfaces)
+    # Get OS_verson
+    # TODO it looks like there is no OS verson available in any YANG model implemnted in Arista 
+    os_version = ""
 
-    # return {
-    #     "hostname": hostname["hostname"], +
-    #     "fqdn": hostname["fqdn"], +
-    #     "vendor": "Arista", +
-    #     "model": version["modelName"],
-    #     "serial_number": version["serialNumber"], 
-    #     "os_version": version["internalVersion"],
-    #     "uptime": int(uptime), 
-    #     "interface_list": interfaces,
-    # }
+    result["os_version"] = os_version
+
+    # Get uptime
+    # Provided in nanoseconds per gNMI spe
+    try:
+        uptime = time.time_ns() - int(data["notification"][0]["update"][0]["val"]["openconfig-system:state"]["boot-time"])
+    except:
+        try: 
+            uptime = time.time_ns() - int(data["notification"][0]["update"][0]["val"]["state"]["boot-time"])
+
+        except:
+            uptime = 0
+
+    result["uptime"] = uptime / 10 ** 9 ## Converting to seconds to match NAPALM
+
+
+    # Get Interfaces list
+    interface_list = []
+    try:
+        for item in data["notification"][2]["update"][0]["val"]["openconfig-interfaces:interface"]:
+            interface_list.append(item["name"])
+    except:
+        try: 
+            for item in data["notification"][2]["update"][0]["val"]["interface"]:
+                interface_list.append(item["name"])
+
+        except:
+            uptime = 0
+
+    result["interface_list"] = interface_list
 
     return result
